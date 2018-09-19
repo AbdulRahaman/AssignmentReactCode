620e8c8c47153f24f01932617ab39ef05f06a4ad
Abdul Rahaman <abdul.rahaman.work@gmail.com>
2018-09-19 15:29:57 +0530

    Added E2E test cases

--- E2E_Test/App.test.js
+++ E2E_Test/App.test.js
@@ -1,38 +1,65 @@
 const faker = require("faker");
 const puppeteer = require("puppeteer");
+const mount = require("enzyme");
 
-const person = {
-  name: faker.name.firstName() + " " + faker.name.lastName(),
-  email: faker.internet.email(),
-  phone: faker.phone.phoneNumber(),
-  message: faker.random.words()
-};
+let browser;
+let page;
 
-describe("H1 Text", () => {
+beforeAll(async () => {
+  // launch browser
+  browser = await puppeteer.launch({
+    headless: false
+  });
+  // creates a new page in the opened browser
+  page = await browser.newPage();
+  page.emulate({
+    viewport: {
+      width: 700,
+      height: 800
+    },
+    userAgent: ""
+  });
+  await page.goto("http://localhost:3000/");
+  await page.waitForSelector(".username");
+});
+
+describe("Search User", () => {
   test(
-    "h1 loads correctly",
+    "Should display a list of users",
     async () => {
-      let browser = await puppeteer.launch({
-        headless: false
+      await page.screenshot({
+        path: "screenshots/users.jpg",
+        fullPage: true
       });
-      let page = await browser.newPage();
-
-      page.emulate({
-        viewport: {
-          width: 500,
-          height: 2400
-        },
-        userAgent: ""
+      const list = await page.evaluate(result => {
+        const usernames = Array.from(document.querySelectorAll(".username"));
+        return usernames.map(user => {
+          const name = user.textContent;
+          return `${name}`;
+        });
       });
-
-      await page.goto("http://localhost:3000/");
-      await page.waitForSelector(".App-title");
-
-      const html = await page.$eval(".App-title", e => e.innerHTML);
-      expect(html).toBe("Welcome to React");
-
-      browser.close();
+      await expect(list.length>0);
     },
     16000
   );
+  test(
+    "Should found a record contains mrs",
+    async () => {
+      await page.type("input[name=search]", "mrs");
+      await page.waitForSelector(".username");
+      const list = await page.evaluate(result => {
+        const usernames = Array.from(document.querySelectorAll(".username"));
+        return usernames.map(user => {
+          const name = user.textContent;
+          return `${name}`;
+        });
+      });
+      await expect(list[0]).toContain("mrs");
+    },
+    16000
+  );
+});
+
+afterAll(() => {
+  browser.close();
 });
